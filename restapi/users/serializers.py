57455d6aa306a168fa73c26dfdb4c78d79f1eb17
opielapatryk <xpatrykopiela@gmail.com>
2024-03-16 11:11:28 +0100

    Ready basic API

--- restapi/users/serializers.py
+++ restapi/users/serializers.py
@@ -1,14 +1,7 @@
-from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+        fields = ['id', 'username']
--- restapi/users/views.py
+++ restapi/users/views.py
@@ -1,22 +1,18 @@
-from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
-
-from users.serializers import GroupSerializer, UserSerializer
+from django.contrib.auth.models import User
+from users.serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'users': reverse('user-list', request=request),
+    })
